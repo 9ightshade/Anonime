0d859357425053a35ede9fac8af0b43d3ef6a9c8
9ightshade <aniekemeusanga@gmail.com>
2024-01-26 21:34:27 +0100

    project set up and installations

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+  <div>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet, veniam.</p>
+  </div>
+  )
+}
+
+export default App
