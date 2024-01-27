5e5a517d9b7d42d0fd8e24a8fc0cd65de76947fb
9ightshade <aniekemeusanga@gmail.com>
2024-01-27 01:22:47 +0100

    anime release component

--- src/components/home/NewRelease.jsx
+++ src/components/home/NewRelease.jsx
@@ -1,11 +1,67 @@
+import aoashi from "../../assets/images/aoashi_card.png"
+import attackontitan from "../../assets/images/attackontitan.png"
+import boruto_naruto_next_generations from "../../assets/images/boruto-naruto-next-generations.png"
+import captsu from "../../assets/images/captsu_card.png"
 export const NewRelease = ()=>{
 return(
 
 
     <div>
-        <h2>New Realease</h2>
+        <h2 className="font-semibold text-[30px] text-white pt-[1em] pb-[.5em]">New Realease</h2>
+        <div className="flex gap-[1em] " >
+        <AnimeNewRelease/>
+        </div>
     </div>
 )
 
 
+}
+
+export const AnimeNewRelease = ()=>{
+
+return(
+<div className="flex gap-[1em]" >
+    
+<div className="text-white" >
+    <div style={{backgroundImage:`url(${attackontitan})`}} className="bg-no-repeat h-[30vh] rounded-[10px] pt-[9em] px-[2em]">
+<h2>Episode 1018</h2>
+    </div>
+    <p className="px-[1em]" >Shingeki no kyoujin</p>
+</div>
+
+
+
+
+
+<div className="text-white" >
+    <div style={{backgroundImage:`url(${aoashi})`}} className="bg-no-repeat h-[30vh] rounded-[10px] pt-[9em] px-[2em]">
+<h2>Episode 1018</h2>
+    </div>
+    <p className="px-[1em]" >Shingeki no kyoujin</p>
+</div>
+
+
+
+
+<div className="text-white" >
+    <div style={{backgroundImage:`url(${boruto_naruto_next_generations})`}} className="bg-no-repeat h-[30vh] rounded-[10px] pt-[9em] px-[2em] "  >
+<h2>Episode 1018</h2>
+    </div>
+    <p className="px-[1em]" >Shingeki no kyoujin</p>
+</div>
+
+
+
+
+
+
+
+<div className="text-white" >
+    <div style={{backgroundImage:`url(${captsu})`}} className="bg-no-repeat h-[30vh] rounded-[10px] pt-[9em] px-[2em] "  >
+<h2>Episode 1018</h2>
+    </div>
+    <p className="px-[1em]" >Shingeki no kyoujin</p>
+</div>
+</div>
+)
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import { Caurosel } from "../components/home/Caurosel"
 import { Explore } from "../components/home/Explore"
+import { NewRelease } from "../components/home/NewRelease"
 import { Navbar } from "../components/shared/Navbar"
 
 export const Home = ()=>{
@@ -7,11 +8,12 @@ export const Home = ()=>{
 
     return(
 
-<div className="bg-[#192026] h-[100vh] " >
+<div className="bg-[#192026]" >
 <Navbar/>
 <div className="w-[80%] mx-auto " >
 <Explore/>
 <Caurosel/>
+<NewRelease/>
 </div>
 </div>
     )
