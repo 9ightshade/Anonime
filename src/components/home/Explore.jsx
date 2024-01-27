098b648f5af8d33cb2afa260a69166deadce5043
9ightshade <aniekemeusanga@gmail.com>
2024-01-27 08:08:15 +0100

    search functionality implementation

--- src/components/home/Explore.jsx
+++ src/components/home/Explore.jsx
@@ -4,8 +4,8 @@ return(
 
 <div className="px-[1em]" >
 
-<h2 className="text-white font-medium" >Explore</h2>
-<input type="text" name="explore" id="explore" placeholder="What are you gonna watch today ?" className="text-[#868686] bg-[#192026] w-[50%] outline-none focus:bg-[#1920300] border-none my-[1em]  " />
+<h2 className="text-white font-medium hover:text-blue-600 duration-150 " >Explore</h2>
+<input type="text" name="explore" id="explore" placeholder="What are you gonna watch today ?" className="text-[#868686] bg-[#192026] w-[50%] outline-none focus:bg-[#1920300] border-none my-[1em]" />
 
 </div>
 
--- src/components/shared/SearchAnime.jsx
+++ src/components/shared/SearchAnime.jsx
@@ -1,11 +1,22 @@
+import { useState } from "react";
+
+
 export const SearchAnime = ()=>{
+ const [searchValue, setSearchValue] = useState("");
+ 
+
+ {/**function handles search query and searches database for match which it returns to caurosel */}
+    const handleSearchQuery =(e)=>{
+setSearchValue(e.target.value);
+console.log(searchValue);
+    }
+
 
 return(
 
 <div>
 
-<input type="search" name="search" id="search" placeholder="Search anime or movie" className="bg-slate-800 rounded-[30px] py-[.7em] pl-[.9em] pr-[9em] text-[#868686] outline-none " />
-
+<input type="search" name="search" id="search" placeholder="Search anime or movie" className="bg-slate-800 rounded-[30px] py-[.7em] pl-[.9em] pr-[9em] text-[#868686] outline-none " onChangeCapture={handleSearchQuery} />
 
 </div>
 
